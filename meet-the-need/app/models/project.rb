4ad0a8539de03ff9eebb26b014686cf573ea8d58
Katie Ok <okdonga@gmail.com>
2015-10-30 14:30:43 -0700

    update helper method in project model

--- meet-the-need/app/models/project.rb
+++ meet-the-need/app/models/project.rb
@@ -8,18 +8,21 @@ class Project < ActiveRecord::Base
 
   validates :title, :body, :customer_id, presence: true
 
+  # find associations between a project's topics and potential developers
   def dev_finder
     our_devs_ids = []
     our_devs = []
-    our_topic_ids = self.project_topics.collect {|project_techs| project_techs.topic_id}
 
+    # iterates through the ids of topics associated with the project the method is called on; returns an array of developer ids
     topic_ids.each do |t_id|
       our_devs_ids << ActiveRecord::Base::DeveloperTopic.where(topic_id: t_id)
     end
+
+    # iterates through developer ids to return an array of developer objects
     our_devs_ids.each do |dev_id|
       our_devs << Developer.where(id: dev_id)
     end
+
     return our_devs
   end
-
 end
