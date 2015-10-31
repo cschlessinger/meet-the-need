545237a127d9cc6ba7fe63f5cde4e587c3082298
Katie OK & Andrew Dowd <pair+okdonga.plisonousbess@devbootcamp.com>
2015-10-31 13:47:23 -0700

    add some more seeds

--- meet-the-need/db/seeds.rb
+++ meet-the-need/db/seeds.rb
@@ -7,12 +7,15 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 Customer.create(first_name: "Bob", last_name: "Smith", email: "bobsmith@example.com", zipcode: "90210")
+Customer.create(first_name: "Zedechiah", last_name: "Smith", email: "zedsmith@example.com", zipcode: "90210")
+Customer.create(first_name: "Monroe", last_name: "Smith", email: "monroesmith@example.com", zipcode: "90210")
+Customer.create(first_name: "Tyler", last_name: "Smith", email: "tysmith@example.com", zipcode: "90210")
 Developer.create(first_name: "Sally", last_name: "Jones", email: "sallyjones@example.com", zipcode: "81239")
 Developer.create(first_name: "Fionnuala", last_name: "Jones", email: "fionnualajones@example.com", zipcode: "81239")
 Developer.create(first_name: "Adeodatus", last_name: "Jones", email: "adeodatusjones@example.com", zipcode: "94110")
 Project.create(title: "LGBT Chat Room", body: "This is a support chat room for LGBT youth", customer_id: 1)
 Project.create(title: "Bowling league", body: "BOWLING!!!!!!!!!!!!!!!", customer_id: 1)
-Project.create(title: "Bulletin board", body: "some shit", customer_id: 1)
+Project.create(title: "Bulletin board", body: "some shit", customer_id: 3)
 
 Technology.create(name: "chat")
 Technology.create(name: "iOS")
