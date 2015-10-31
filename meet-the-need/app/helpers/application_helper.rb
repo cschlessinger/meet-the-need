822c125920d6b2405a830fcac5076371c5ffa8ce
cschlessinger <claire.schlessinger@gmail.com>
2015-10-31 15:11:18 -0700

    Get concern method in truly working order

--- meet-the-need/app/helpers/application_helper.rb
+++ meet-the-need/app/helpers/application_helper.rb
@@ -1,17 +1,3 @@
 module ApplicationHelper
 
-# find associations between a project's topics and potential developers
-# matcher is what you are starting with (eg instance of Project)
-# matchee is the class you are matching against (eg. Developer)
-
-
-
 end
-
-# DeveloperTopic.first.finder(Project.first, "topics", "Developer")
-# DeveloperTopic.first.finder(Project.first, "technologies", "Developer")
-
-
-# dev_finder('Developer.first', "topic", "Project")
-# dev_finder('Project.first', "technology", "Developer")
-# dev_finder('Developer.first', "technology", "Project")
