ccee97ad788e13bd9f4e2e2a7ed34de5cfcfd42a
Katie OK & Andrew Dowd <pair+okdonga.plisonousbess@devbootcamp.com>
2015-10-31 17:11:35 -0700

    refactor customer and dev partial and move some logic to controller

--- meet-the-need/app/controllers/homepage_controller.rb
+++ meet-the-need/app/controllers/homepage_controller.rb
@@ -1,5 +1,12 @@
 class HomepageController < ApplicationController
   def index
-    @projects = Project.all
+    projects = Project.all
+    reversed_projects = projects.reverse
+    @latest_three_projects = reversed_projects.slice!(0..2)
+
+    developers = Developer.all
+    reversed_developers = developers.reverse
+    @latest_three_developers = reversed_developers.slice!(0..2)
+
   end
 end
