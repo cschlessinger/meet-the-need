818d4216db4f9879f89c9a209984630c31970e84
Katie Ok <okdonga@gmail.com>
2015-11-01 15:06:53 -0800

    list top three matching developer for first unassigned project

--- meet-the-need/app/controllers/customers_controller.rb
+++ meet-the-need/app/controllers/customers_controller.rb
@@ -12,12 +12,21 @@ class CustomersController < ApplicationController
   # GET /customers/1
   # GET /customers/1.json
   def show
-    all_projects = set_customer.projects
-    if all_projects.length <= 3
-      @unassigned_projects = all_projects
-    else
-      @unassigned_projects = all_projects.where(developer_id: nil).slice!(0..2)
+
+    all_customer_projects = set_customer.projects
+    @unassigned_projects = all_customer_projects.where(developer_id: nil).slice(0..2)
+
+    # Find top three matching developers by topic and technology, put them into an array and return the top three unique results.
+    matching_three_developers_by_topic = all_customer_projects.first.finder(all_customer_projects.first, "topics", "Developer")
+    matching_three_developers_by_technology = all_customer_projects.first.finder(all_customer_projects.first, "technologies", "Developer")
+    all_matching_developers = []
+    all_matching_developers << matching_three_developers_by_technology << matching_three_developers_by_topic
+    @internal_results = []
+    all_matching_developers.each do |matching_developer|
+      @internal_results << matching_developer
     end
+    @top_unique_matching_three = @internal_results.flatten!.uniq.slice(0..2)
+
   end
 
   # GET /customers/new
