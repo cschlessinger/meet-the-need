3103d5f549e49d3dbcb8bb6c315d02a2861224ac
Katie Ok <okdonga@gmail.com>
2015-11-02 14:53:52 -0800

    got avatar images working, started cust profile form, included bio column for customer in migration

--- meet-the-need/db/migrate/20151030013408_create_developers.rb
+++ meet-the-need/db/migrate/20151030013408_create_developers.rb
@@ -3,8 +3,6 @@ class CreateDevelopers < ActiveRecord::Migration
     create_table :developers do |t|
       t.string :first_name
       t.string :last_name
-      # t.string :email
-      # t.string :password
       t.string :zipcode
       t.string :tagline
       t.text :bio
--- meet-the-need/db/schema.rb
+++ meet-the-need/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151102005125) do
+ActiveRecord::Schema.define(version: 20151102223100) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20151102005125) do
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
+    t.text     "bio"
   end
 
   add_index "customers", ["email"], name: "index_customers_on_email", unique: true, using: :btree
