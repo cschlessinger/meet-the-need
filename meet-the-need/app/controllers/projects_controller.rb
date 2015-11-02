c8e08d7065baaea6e862f7fb80d79e92f9868c05
Katie Ok <okdonga@gmail.com>
2015-11-02 10:17:03 -0800

    Clean up html on unassigned projects page, tweak create new project form, get unassigned project ajax working, convert see more details button to link, show request button only if developer

--- meet-the-need/app/controllers/projects_controller.rb
+++ meet-the-need/app/controllers/projects_controller.rb
@@ -71,7 +71,7 @@ class ProjectsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def project_params
-      params.require(:project).permit(:title, :body)
+      params.require(:project).permit(:title, :body, :cost)
       # params[:project]
     end
 
