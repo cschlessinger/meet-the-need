be3a9a9c20396094d7eecf96483a3be78bd89797
Sasha Goldenson <sgoldens@gmail.com>
2015-11-05 11:43:09 -0800

    fix error you get when a new customer signs up

--- meet-the-need/app/controllers/customers_controller.rb
+++ meet-the-need/app/controllers/customers_controller.rb
@@ -67,7 +67,7 @@ class CustomersController < ApplicationController
 
     respond_to do |format|
       if @customer.save
-        format.html { redirect_to @customer, notice: 'Customer was successfully created.' }
+        format.html { redirect_to "#{@customer}#profile", notice: 'Customer was successfully created.' }
         format.json { render :show, status: :created, location: @customer }
       else
         format.html { render :new }
