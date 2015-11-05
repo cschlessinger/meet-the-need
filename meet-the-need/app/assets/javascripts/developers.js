df5451b1bd407461b1308247e5cb232f09467462
cschlessinger <claire.schlessinger@gmail.com>
2015-11-05 10:44:12 -0800

    Revert "add conversation indicator, make button green"
    This reverts commit 78b3c9a251e5876a8103b2b98046f83d77414c58.

--- meet-the-need/app/assets/javascripts/developers.js
+++ meet-the-need/app/assets/javascripts/developers.js
@@ -4,9 +4,7 @@ $(function() {
 
 $(document).ready(function() {
   $('.message-content:first').addClass('active');
-  $('.select-conversation:first').addClass('active-indicator')
   $('.select-conversation').click(function() {
-  	this.addClass('active-indicator')
   	this_id = $(this).attr('id');
   	console.log(this_id);
   	$('.active').removeClass('active');
