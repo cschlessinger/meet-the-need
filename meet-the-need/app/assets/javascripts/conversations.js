37307a3f6f71cd3bb68bfb15af224aa8f7a4eba2
cschlessinger <claire.schlessinger@gmail.com>
2015-11-05 09:43:02 -0800

    move JS files into conversation.js

--- meet-the-need/app/assets/javascripts/conversations.js
+++ meet-the-need/app/assets/javascripts/conversations.js
@@ -1,2 +1,12 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(document).ready(function() {
+  $('.message-content:first').addClass('active');
+  $('.select-conversation').click(function() {
+  	this_id = $(this).attr('id');
+  	console.log(this_id);
+  	$('.active').removeClass('active');
+  	$('#' + this_id + '.message-content').addClass('active');
+  })
+})
