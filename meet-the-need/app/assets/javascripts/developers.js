78b3c9a251e5876a8103b2b98046f83d77414c58
cschlessinger <claire.schlessinger@gmail.com>
2015-11-04 23:39:13 -0800

    add conversation indicator, make button green

--- meet-the-need/app/assets/javascripts/developers.js
+++ meet-the-need/app/assets/javascripts/developers.js
@@ -4,7 +4,9 @@ $(function() {
 
 $(document).ready(function() {
   $('.message-content:first').addClass('active');
+  $('.select-conversation:first').addClass('active-indicator')
   $('.select-conversation').click(function() {
+  	this.addClass('active-indicator')
   	this_id = $(this).attr('id');
   	console.log(this_id);
   	$('.active').removeClass('active');
