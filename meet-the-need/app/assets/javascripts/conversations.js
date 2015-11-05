baf630252b9ff36f27cf8adbf8a2c78b7b47ce01
cschlessinger <claire.schlessinger@gmail.com>
2015-11-05 09:50:49 -0800

    add color indicator for which convo, green button for create message

--- meet-the-need/app/assets/javascripts/conversations.js
+++ meet-the-need/app/assets/javascripts/conversations.js
@@ -3,10 +3,12 @@
 
 $(document).ready(function() {
   $('.message-content:first').addClass('active');
+  $('.select-conversation:first').addClass('active-selector')
   $('.select-conversation').click(function() {
   	this_id = $(this).attr('id');
-  	console.log(this_id);
   	$('.active').removeClass('active');
+  	$(this).addClass('active-selector');
+  	$(this).siblings('.select-conversation').removeClass('active-selector')
   	$('#' + this_id + '.message-content').addClass('active');
   })
 })
